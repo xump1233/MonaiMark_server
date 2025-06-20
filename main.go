07e2954da3df8a3adce60de1420cb5f29748d1a8
xump <2225338553@qq.com>
2025-06-20 14:21:42 +0800

    feat:通用web资源服务器

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
-	// 设置静态文件服务：将 dist 目录映射为网站根路径 /
-	r.StaticFS("/", http.Dir("static/dist"))
+	staticDir := "static"
+
+	// 读取 static 目录下的所有子目录
+	files, err := os.ReadDir(staticDir)
+	if err != nil {
+		panic(fmt.Sprintf("无法读取 static 目录: %v", err))
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			subDir := file.Name()
+			// 注册路由：例如 /mark -> static/mark/dist
+			r.StaticFS("/"+subDir, http.Dir(staticDir+"/"+subDir+"/dist"))
+			fmt.Printf("已注册路由: /%s -> %s/%s/dist\n", subDir, staticDir, subDir)
+		}
+	}
 
 	// 启动服务
+	fmt.Println("服务启动中，监听 :8081")
 	if err := r.Run(":8081"); err != nil {
 		panic(err)
 	}
